a352f4e333db0c3eb637b5c8b5788c104afcac28
REKA157 <riqa890@gmail.com>
2025-05-23 18:54:05 +0200

    feat: Configure i18next to load translations via HTTP backend

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import i18nextHttpBackend from 'i18next-http-backend';
 
 // the translations
 // (tip: often other resources are loaded from a backend server or a file system, like cdn)
@@ -17,11 +18,15 @@ const resources = {
 };
 
 i18n
+  .use(i18nextHttpBackend) // passes i18n down to react-i18next
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
     lng: "fr", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     fallbackLng: "en", // fallback language is used when lng not found
+
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json' // adjusted path to load from public/locales
+    },
 
     interpolation: {
       escapeValue: false // react already safes from xss
