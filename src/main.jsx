87f8f970b93eb35fb7530f371266c916e3d37217
VotreNom <VotreEmail@example.com>
2025-05-31 21:03:33 +0200

    Ensure proper Leaflet CSS import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './i18n'; // Importez votre fichier de configuration i18n
+import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
